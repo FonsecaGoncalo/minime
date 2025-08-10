c1425712dde91b698b271d7d6c30423d881d3d13
goncalofonseca <goncalofnsc@gmail.com>
2025-08-10 15:33:58 +0100

    remove not needed files from the bundle

--- backend/build.sh
+++ backend/build.sh
@@ -9,6 +9,16 @@ rm -f requirements.txt
 poetry export --without-hashes --format=requirements.txt > requirements.txt
 pip install -r requirements.txt -t dist/
 
+# Remove dep
+find dist -path "*/googleapiclient/discovery_cache" -type d -exec rm -rf {} +
+find dist -type d -name "__pycache__" -exec rm -rf {} +
+find dist -type d \( -name "tests" -o -name "test" -o -name "testing" -o -name "examples" \) -exec rm -rf {} +
+find dist -type d -name ".pytest_cache" -exec rm -rf {} +
+find dist -type f \( -name "*.pyc" -o -name "*.pyo" -o -name "*.whl" \) -delete
+find dist -type d -name "discovery_cache" -exec rm -rf {} +
+find dist -type f \( -name "README*" -o -name "CHANGELOG*" -o -name "LICENSE*" \) -delete
+
+
 # Copy source code into the dist directory
 cp -r -v app/* dist
 
